b772208e69dfd5009c6edaf0d1386d5a0aa2d0f8
Dmitry Smetankin <smetankinda@gmail.com>
2023-05-18 16:39:42 +0300

    deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Даже получилось изменить фразу!')
 
 
 def second_page(request):
